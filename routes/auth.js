dcabcbe34dd9fbd5c7bd0b81f9155d78cdb33f6b
Munem223 <munemshahriar223@gmail.com>
2026-05-22 21:03:10 +0600

    final push

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
-const { registerUser, loginUser, googleLogin } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.post('/google', googleLogin);     // ← This line must be here
 
 module.exports = router;
